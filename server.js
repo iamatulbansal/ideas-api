4568d9c3746bfdb919d3b0f4fc2c9d5bf5474fbe
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-04-21 20:29:24 +0530

    setup folder structure move all folder inside src folder

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const middleware = require("./middleware/idea.middleware");
+const middleware = require("./src/middleware/idea.middleware");
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 app.use(middleware);
 
-require("./routes/idea.route")(app);
+require("./src/routes/idea.route")(app);
 
 app.listen(port, () => {
   console.log("Server is running");
